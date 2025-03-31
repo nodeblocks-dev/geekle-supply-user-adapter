2ed07b8823544e3ec13c93694a61621f9006dfee
Oleh <oleh@basal.dev>
2025-03-31 14:28:58 +0900

    ✨ feature: update invite user email template with new contact information and formatting

--- src/emails/templates/inviteUser.ts
+++ src/emails/templates/inviteUser.ts
@@ -5,15 +5,16 @@ export const inviteUserTemplate = {
     bodyTemplate: `
       <p>Geekleをご利用いただきありがとうございます。</p>
       <p>以下のリンクからアカウント作成を行ってください</p>
+      
       <p>[<a href="\${url}">アカウント作成URL</a>]</p>
+
       <p>本メールは送信専用のアドレスです。</p>
       <p>このメールに心当たりがない場合は、お問い合わせください。</p>
-      <br />
-      <p>Geekle</p>
-      <a href="${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL', '')}">${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL', '')}</a>
-      <br />
-      <br />
-      <p>運営会社 Geekle</p>
+      
+      <p>-------------------</p>
+      <p>Geekle 運営事務局</p>
+      <p>141-0021 東京都品川区上大崎2-15-19</p>
+      <p>お問い合わせ メールアドレス ： contact@geekle.com</p>
     `,
     subject: '[Geekle] アカウントの登録を行います',
     urlTemplate: `${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL', '')}` + '/auth/accept-invitation/${invitationId}/${token}?email=${email}'
