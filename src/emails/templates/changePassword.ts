2a7c956e0bfc0354297d1e9790cfa396d6e76316
Oleh <oleh@basal.dev>
2025-04-03 17:00:24 +0900

    ✨ feature: enhance change password and verify change email templates with updated contact information and improved formatting

--- src/emails/templates/changePassword.ts
+++ src/emails/templates/changePassword.ts
@@ -4,18 +4,20 @@ export const changePasswordTemplate = {
   vendor: {
     bodyTemplate: `
       <p>Geekleをご利用いただきありがとうございます。</p>
-      <br />
       <p>パスワードの変更が完了しました。</p>
       <br />
       <p>この変更に覚えがない場合は、速やかに以下のリンクからパスワードを再設定してください</p>
       <br />
-      <a href="${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL')}/settings/change-password">${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL')}/settings/change-password</a>
+      <a href="${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL')}/settings/change-password">[メールアドレス変更URL]</a>
       <br />
       <br />
       <p>本メールは送信専用のアドレスです。</p>
       <p>このメールに心当たりがない場合は、お問い合わせください。</p>
       <br />
-      <p>運営会社 Geekle</p>
+       -------------------
+      <p>Geekle 運営事務局</p>
+      <p>141-0021 東京都品川区上大崎2-15-19</p>
+      <p>お問い合わせ メールアドレス ： contact@geekle.com</p>
     `,
     subject: '[Geekle] パスワードの変更が完了しました',
     urlTemplate: ''
--- src/emails/templates/verifyChangeEmail.ts
+++ src/emails/templates/verifyChangeEmail.ts
@@ -3,21 +3,23 @@ import { getEnvBool, getEnvString } from "../../helpers";
 export const verifyChangeEmailTemplate = {
   vendor: {
     bodyTemplate: `
-      <p>メールアドレスの更新リクエストが送信されました。</p>
+      <p>Geekleをご利用いただきありがとうございます。</p>
+      <p>このメールはメールアドレスの変更を依頼された方にお送りしています</p>
       <p>以下のURLにアクセスし、メールアドレスの変更を完了してください。</p>
-      <p><a href="\${url}">\${url}</a></p>
-      
-      <p>※このメールは送信専用のメールアドレスから送信されています。</p>
-      <p>このメールに返信することはできません。ご了承ください。</p>
       <br />
-      <p>Geekle</p>
-      <a href="${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL')}">${getEnvString('ADAPTER_CUSTOM_SUPPLY_SITE_URL')}</a>
+      <p>[<a href="\${url}">メールアドレス変更URL</a>]</p>
+      <p>パスワードの設定は、このメールの受信から24時間以内に実施してください。</p>
       <br />
+      <p>本メールは送信専用のアドレスです。</p>
+      <p>このメールに心当たりがない場合は、お問い合わせください。</p>
       <br />
-      <p>運営会社 Geekle</p>
+       -------------------
+      <p>Geekle 運営事務局</p>
+      <p>141-0021 東京都品川区上大崎2-15-19</p>
+      <p>お問い合わせ メールアドレス ： contact@geekle.com</p>
     `,
-    subject: 'メールアドレス認証のお願い',
-    urlTemplate: ''
+    subject: '[Geekle] メールアドレスの変更を行います',
+    urlTemplate: `${getEnvString('ADAPTER_CUSTOM_ADMIN_SITE_URL')}/settings/verify-change-email-success/` + '${token}'
   },
   enabled: getEnvBool('ADAPTER_EMAIL_VERIFY_CHANGE_EMAIL_ENABLED')
 }
